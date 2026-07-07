a53cd45efa744d79ad4dd91b89e43bdd8ea1326f
Black Jesus <rootsofharnoor@gmail.com>
2026-07-07 20:18:04 +0530

    Added the Parlay Tab

---
+++ pages/Parlay.py
@@ -0,0 +1,119 @@
+import streamlit as st
+import pandas as pd   
+import sqlite3
+import numpy as np
+st.set_page_config(page_title="Parlay Tab",layout="wide")
+cn = sqlite3.connect(r"data\events.db")
+cs = cn.cursor()
+df = pd.DataFrame()
+
+
+with st.sidebar:
+    st.title("Current Bankroll")
+    st.metric(label="Current money",value=f"${st.session_state.bankroll:,.2f}")
+    
+    
+####Bankroll
+path = r"data\bankroll.txt"
+        
+def write_money():
+    with open(path,"w") as w:
+        w.write(str(st.session_state.bankroll))
+    with open('data\log_roll.txt',"a") as a:
+        a.write(str(st.session_state.bankroll)+"\n")    
+#######
+if "deleted" not in st.session_state:
+    st.session_state.deleted = ":red[Entry Deleted]"
+if "del_ind" not in st.session_state:
+    st.session_state.del_ind = set()
+if st.session_state.parlay:
+    placeholder = ",".join(["?"]*len(st.session_state.parlay))
+    sql_query = f"SELECT * FROM EVENTS WHERE game_id IN ({placeholder})"
+    df = pd.read_sql_query(sql_query,cn,params=st.session_state.parlay)
+    df = df.drop_duplicates(subset=["game_id"], keep="first")
+else:
+    st.write(":red[Empty Parlay]")
+bet_cal = []
+if not df.empty:   
+    for idx,row in enumerate(df.itertuples()):
+        if idx in st.session_state.del_ind:
+            continue
+        row_header = f"⏰ {row.time},{row.date} | {row.home_team} vs {row.away_team}"
+        with st.expander(label=row_header,expanded=False):
+            c1,c2 = st.columns([3,1])
+            with c1:
+                st.markdown(f"**📍 Venue:** {row.venue}")
+                st.markdown(
+                    f"📊 **Market Odds:** Home: `{row.odds_1}` | Away: `{row.odds_y}` "
+                )
+                
+                choice = st.radio(
+                    "Select the Bet:",
+                    [row.home_team,row.away_team],
+                    key = f"r_{row.game_id}"
+                )
+                if idx in st.session_state.del_ind:
+                    bet_cal.append([idx, choice, row.odds_1 if choice == row.home_team else row.odds_y, row.WINNER, "Deleted"])
+                else:    
+                    bet_cal.append([idx,choice,row.odds_1 if choice == row.home_team else row.odds_y,row.WINNER])
+                
+                    
+            with c2:
+                if st.button("Delete Entry",key=f"k_{row.game_id}"):
+                    st.write(st.session_state.deleted)
+                    st.session_state.del_ind.add(idx)
+                    st.rerun()
+cn.commit()
+cs.close()
+cm = sqlite3.connect(r"data\bets.db")
+cr = cm.cursor()
+cr.execute("""
+           CREATE TABLE IF NOT EXISTS PARLAY(
+               BET REAL,
+               PAYOUT REAL,
+               WON TEXT,
+               LEGS INTEGER     
+           )  
+           """)
+bet_cal = [sub for sub in bet_cal if len(sub)==4]
+bet_cal = np.array(bet_cal,dtype=object)
+row_odds = 1.00   
+money_bet = st.number_input(
+                "Bet Wager:",
+                min_value=0.00,
+                max_value=float(st.session_state.bankroll),
+                step= 5.00
+)                 
+if st.button("Confirm Parlay"):
+    if bet_cal.size>0:
+        for i in range(len(bet_cal)):
+            if bet_cal[i][3] == bet_cal[i][1]:
+                row_odds = row_odds*float(bet_cal[i][2])
+            else:
+                st.write(":red[Parlay Lost]")
+                st.write(f"*** Match {i+1}::")
+                st.write(f"Choice:{bet_cal[i][1]}")
+                st.write(f"Winner:{bet_cal[i][3]}")
+                row_odds = 0.00
+        sql_query = """
+        INSERT INTO PARLAY(BET,PAYOUT,WON,LEGS)
+        VALUES(?,?,?,?)
+        """
+        cr.execute(sql_query,(money_bet
+                              ,row_odds*money_bet
+                              ,"True" if row_odds != 0 else "False",
+                                len(bet_cal)
+                   ))
+        cm.commit()
+        cr.close()
+        if row_odds != 0.00:
+            st.write(":green[Parlay Won]")
+            st.write(f"Total Payout: {row_odds*money_bet:.2f}$")
+            st.session_state.bankroll = st.session_state.bankroll + (row_odds*money_bet)
+            write_money()
+        else:
+            st.write("Total Payout: 0$")
+            st.session_state.bankroll  = st.session_state.bankroll - money_bet
+            write_money()
+    else:
+        st.write("Check Parlay again")
