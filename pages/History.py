e20df7b2d4dd39063048aafc835ae0511e59ca86
Black Jesus <rootsofharnoor@gmail.com>
2026-07-13 21:06:44 +0000

    added slip functionality

--- pages/History.py
+++ pages/History.py
@@ -1,8 +1,13 @@
 from pathlib import Path
-
 import pandas as pd
 import sqlite3
 import streamlit as st
+from jinja2 import Environment, FileSystemLoader
+from weasyprint import HTML
+from datetime import datetime
+import string
+import random 
+import os 
 from utils.styling import inject_custom_css
 inject_custom_css()
 st.set_page_config(page_title="History", layout="wide")
@@ -12,6 +17,10 @@ DATA_DIR = ROOT_DIR / "data"
 STATIC_DIR = ROOT_DIR / "static"
 DATA_DIR.mkdir(exist_ok=True)
 
+def generate_ticket_id(length=8):
+    characters = string.ascii_uppercase + string.digits
+    random_part = ''.join(random.choices(characters, k=length))
+    return f"TRB-{random_part}"
 
 st.markdown('<div class="monocraft-app">', unsafe_allow_html=True)
 
@@ -52,6 +61,27 @@ if not bets_df.empty:
                 st.markdown(f"Winner: {row.winner}")
             with c2:
                 st.markdown(f"Total Payout: {row.payout}$")
+                if st.button("Generate Betting Slip",key=f"bet_{row.winner}_{row.Index}"):
+                    pages_dir = os.path.dirname(os.path.abspath(__file__))
+                    env = Environment(loader=FileSystemLoader(pages_dir))
+                    template = env.get_template("the-tribune-straight-bet.html")
+                    html_content = template.render(
+                        ticket_id = generate_ticket_id(),
+                        date_time = datetime.today().strftime('%Y-%m-%d'),
+                        team_1 = row.home_team,
+                        team_2 = row.away_team,
+                        choice = row.team_bet_on,
+                        odds = "BAD FOR YOU" if row.team_bet_on  != row.winner else float(round(row.payout/row.bet_amount,2)),
+                        money_bet = f"${row.bet_amount}",
+                        expected_payout = f"${round(row.payout,2)}" if row.team_bet_on == row.winner else "$0"
+                    )
+                    pdf_bytes = HTML(string=html_content).write_pdf()
+                    st.download_button(
+                        label="📥 Download Slip PDF",
+                        data=pdf_bytes,
+                        file_name=f"betting_slip.pdf",
+                        mime="application/pdf"
+                    )
 else:
     st.write("No bet history available.")
 
@@ -65,6 +95,26 @@ if not parlay_df.empty:
             with m1:
                 st.write(f"Amount Bet: {row.BET}$")
                 st.write(f"Payout: {row.PAYOUT:.2f}$")
+            with m2:
+                if st.button("Generate Betting Slip",key=f"bet_{row.PAYOUT}_{row.Index}"):
+                    pages_dir = os.path.dirname(os.path.abspath(__file__))
+                    env = Environment(loader=FileSystemLoader(pages_dir))
+                    template = env.get_template("the-tribune-parlay.html")
+                    html_content = template.render(
+                        legs_1 = row.LEGS,
+                        ticket_id = generate_ticket_id(),
+                        date = datetime.today().strftime('%Y-%m-%d'),
+                        legs = row.LEGS,
+                        amount_bet = f"${row.BET}",
+                        payout = f"${float(round(row.PAYOUT,2))}"
+                    )
+                    pdf_bytes = HTML(string=html_content).write_pdf()
+                    st.download_button(
+                        label="📥 Download Slip PDF",
+                        data=pdf_bytes,
+                        file_name=f"betting_slip.pdf",
+                        mime="application/pdf"
+                    )
 else:
     st.write("No parlay history available.")
 
